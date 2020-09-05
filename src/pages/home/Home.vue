8d835ba3afa7b5abbd2dc88c4911320ce62b657e
Mikonnnn <1551005987@qq.com>
2020-09-05 15:05:43 +0800

    新增首页header区域

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+	<home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+export default {
+  name: 'HelloWorld',
+  components: {
+  	HomeHeader
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+</style>
